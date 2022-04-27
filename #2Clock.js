26ab81d964cbdfbeafe8a66963fcc1dcf6faf6cb
Nate Trichanh <natetrichanh@gmail.com>
2022-04-26 18:07:31 -0700

    #2Clock debug

--- #2Clock.js
+++ #2Clock.js
@@ -1,13 +1,4 @@
 function past(h, m, s) {
-    let result = 0;
-    if (h) {
-        result += 3600000;
-    }
-    if (m) {
-        result += 60000;
-    }
-    if (s) {
-        result += 1000;
-    }
+    result = (3600000 * h) + (60000 * m) + (1000 * s);
     return result;
 }
